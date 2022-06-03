b09b927b46d4788c39a005dda46867a13b84e8cf
Ying-Ruei Liang (TheKK) <thumbd03803@gmail.com>
2022-06-03 20:43:48 +0800

    Repl: now support multi-line input

--- app/OurScheme/Repl.hs
+++ app/OurScheme/Repl.hs
@@ -1,3 +1,5 @@
+{-# LANGUAGE FlexibleContexts #-}
+{-# LANGUAGE LambdaCase #-}
 {-# LANGUAGE OverloadedStrings #-}
 
 module OurScheme.Repl
@@ -10,6 +12,8 @@ import Control.Exception
 import Control.Monad
 import Control.Monad.Error.Class
 import Control.Monad.IO.Class
+import Data.Foldable
+import Data.Function
 import qualified Data.Text as T
 import qualified Data.Text.IO as T
 import OurScheme.Eval
@@ -21,13 +25,35 @@ repl = do
   env <- blankEnv
   flip runEnvWith env . forever $ do
     liftIO $ T.putStr "> "
-    cmd <- liftIO T.getLine
-    unless (T.all (== ' ') cmd) $ do
-      case runParser pSExp "repl" cmd of
-        Left err -> liftIO $ T.putStrLn $ "Error: " <> T.pack (errorBundlePretty err)
-        Right sexp -> do
-          ret <- tryError $ topLevelNormalize sexp
-          liftIO $ T.putStrLn $ T.pack (show ret)
+    result <- incrementalParsing
+    case result of
+      Left err -> do
+        liftIO $ T.putStrLn $ "Error: " <> T.pack (errorBundlePretty err)
+        liftIO $ print $ isEndOfInputError err
+      Right sexp -> do
+        ret <- tryError $ topLevelNormalize sexp
+        liftIO $ T.putStrLn $ T.pack (show ret)
+  where
+    incrementalParsing = incrementalParsing' ""
+
+    incrementalParsing' prevCmd = do
+      cmd <- (prevCmd <>) <$> liftIO T.getLine
+      case runParser (pSExp <* eof) "repl" cmd of
+        Left err ->
+          if isEndOfInputError err
+            then incrementalParsing' (cmd <> "\n")
+            else pure $ Left err
+        Right sexp -> pure $ Right sexp
 
 tryError :: (MonadError e m) => m a -> m (Either e a)
 tryError m = fmap Right m `catchError` (pure . Left)
+
+isEndOfInputError :: (Eq (Token t)) => ParseErrorBundle t e -> Bool
+isEndOfInputError (ParseErrorBundle parseErrors _) =
+  or $
+    fmap
+      ( \case
+          (TrivialError _ unexpected _) -> EndOfInput `elem` toList unexpected
+          (FancyError _ _) -> False
+      )
+      $ parseErrors
