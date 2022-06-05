fbcb1691700ab6d8bb51c6ff5a29292082b5d05d
Ying-Ruei Liang (TheKK) <thumbd03803@gmail.com>
2022-06-05 16:51:09 +0800

    Builtins: impl '+', '*' & '/'

--- app/OurScheme/Builtins.hs
+++ app/OurScheme/Builtins.hs
@@ -14,19 +14,16 @@ import OurScheme.AST
 stdBuiltins :: [(Symbol, SExp)]
 stdBuiltins =
   fmap SBuiltin
-    <$> [ (Symbol "+", builtinAdd),
+    <$> [ (Symbol "+", builtinFoldInts sum),
+          (Symbol "*", builtinFoldInts product),
           (Symbol "-", builtinMinus),
+          (Symbol "/", builtinDivision),
           (Symbol "<", builtinBinaryCondition (<)),
           (Symbol "<=", builtinBinaryCondition (<=)),
           (Symbol ">", builtinBinaryCondition (>)),
           (Symbol ">=", builtinBinaryCondition (>=)),
           (Symbol "=", builtinBinaryCondition (==))
         ]
-
-builtinAdd :: BuiltinsImpl
-builtinAdd = BuiltinsImpl $ \args' -> do
-  args <- mapM getInt args'
-  pure $ SLit $ LitInt $ sum args
 
 builtinMinus :: BuiltinsImpl
 builtinMinus = BuiltinsImpl $ \args' -> do
@@ -37,6 +34,22 @@ builtinMinus = BuiltinsImpl $ \args' -> do
         [] -> 0
         [n] -> - n
         n : ns -> foldl' (-) n ns
+
+builtinDivision :: BuiltinsImpl
+builtinDivision = BuiltinsImpl $ \args' -> do
+  args <- mapM (fmap fromIntegral . getInt) args'
+  ret <- case args of
+    [] -> Left "wrong number of arguments"
+    [n] -> pure $ 1 / n
+    n : ns -> pure $ foldl' (/) n ns
+  if isInfinite ret
+    then Left "arithmetic error, divide by zero"
+    else pure $ SLit $ LitInt $ floor ret
+
+builtinFoldInts :: ([Int] -> Int) -> BuiltinsImpl
+builtinFoldInts f = BuiltinsImpl $ \args' -> do
+  args <- mapM getInt args'
+  pure $ SLit $ LitInt $ f args
 
 builtinBinaryCondition :: (Int -> Int -> Bool) -> BuiltinsImpl
 builtinBinaryCondition f = builtinBinaryArithmetic $ \a b ->
